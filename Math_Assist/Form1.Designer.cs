f9f55959ec71b061a11a281c3d0e23c7b312be06
Caleb E. Grebill <42920219+TechMaster5@users.noreply.github.com>
2021-02-10 06:03:07 -1000

    Uploaded Source
    Uploaded the source code of the project.

---
+++ Math_Assist/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿
+namespace Math_Assist
+{
+    partial class Math_Assist
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbox_X1 = new System.Windows.Forms.TextBox();
+            this.txtbox_Y1 = new System.Windows.Forms.TextBox();
+            this.txtbox_XM = new System.Windows.Forms.TextBox();
+            this.txtbox_YM = new System.Windows.Forms.TextBox();
+            this.txtbox_X2 = new System.Windows.Forms.TextBox();
+            this.txtbox_Y2 = new System.Windows.Forms.TextBox();
+            this.btn_Midpoint = new System.Windows.Forms.Button();
+            this.btn_Distance = new System.Windows.Forms.Button();
+            this.btn_Endpoint_1 = new System.Windows.Forms.Button();
+            this.btn_Endpoint_2 = new System.Windows.Forms.Button();
+            this.btn_About = new System.Windows.Forms.Button();
+            this.lbl_Endpoint_1 = new System.Windows.Forms.Label();
+            this.lbl_Midpoint = new System.Windows.Forms.Label();
+            this.lbl_Endpoint_2 = new System.Windows.Forms.Label();
+            this.lbl_Distance = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Coordinates 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Midpoint:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Coordinates 2:";
+            // 
+            // txtbox_X1
+            // 
+            this.txtbox_X1.Location = new System.Drawing.Point(93, 6);
+            this.txtbox_X1.Name = "txtbox_X1";
+            this.txtbox_X1.Size = new System.Drawing.Size(40, 20);
+            this.txtbox_X1.TabIndex = 3;
+            this.txtbox_X1.Text = "X1";
+            // 
+            // txtbox_Y1
+            // 
+            this.txtbox_Y1.Location = new System.Drawing.Point(139, 6);
+            this.txtbox_Y1.Name = "txtbox_Y1";
+            this.txtbox_Y1.Size = new System.Drawing.Size(40, 20);
+            this.txtbox_Y1.TabIndex = 4;
+            this.txtbox_Y1.Text = "Y1";
+            // 
+            // txtbox_XM
+            // 
+            this.txtbox_XM.Location = new System.Drawing.Point(93, 32);
+            this.txtbox_XM.Name = "txtbox_XM";
+            this.txtbox_XM.Size = new System.Drawing.Size(40, 20);
+            this.txtbox_XM.TabIndex = 5;
+            this.txtbox_XM.Text = "XM";
+            // 
+            // txtbox_YM
+            // 
+            this.txtbox_YM.Location = new System.Drawing.Point(139, 32);
+            this.txtbox_YM.Name = "txtbox_YM";
+            this.txtbox_YM.Size = new System.Drawing.Size(40, 20);
+            this.txtbox_YM.TabIndex = 6;
+            this.txtbox_YM.Text = "YM";
+            // 
+            // txtbox_X2
+            // 
+            this.txtbox_X2.Location = new System.Drawing.Point(93, 58);
+            this.txtbox_X2.Name = "txtbox_X2";
+            this.txtbox_X2.Size = new System.Drawing.Size(40, 20);
+            this.txtbox_X2.TabIndex = 7;
+            this.txtbox_X2.Text = "X2";
+            // 
+            // txtbox_Y2
+            // 
+            this.txtbox_Y2.Location = new System.Drawing.Point(139, 58);
+            this.txtbox_Y2.Name = "txtbox_Y2";
+            this.txtbox_Y2.Size = new System.Drawing.Size(40, 20);
+            this.txtbox_Y2.TabIndex = 8;
+            this.txtbox_Y2.Text = "Y2";
+            // 
+            // btn_Midpoint
+            // 
+            this.btn_Midpoint.Location = new System.Drawing.Point(139, 84);
+            this.btn_Midpoint.Name = "btn_Midpoint";
+            this.btn_Midpoint.Size = new System.Drawing.Size(40, 20);
+            this.btn_Midpoint.TabIndex = 9;
+            this.btn_Midpoint.Text = "M";
+            this.btn_Midpoint.UseVisualStyleBackColor = true;
+            this.btn_Midpoint.Click += new System.EventHandler(this.btn_Midpoint_Click);
+            // 
+            // btn_Distance
+            // 
+            this.btn_Distance.Location = new System.Drawing.Point(93, 84);
+            this.btn_Distance.Name = "btn_Distance";
+            this.btn_Distance.Size = new System.Drawing.Size(40, 20);
+            this.btn_Distance.TabIndex = 10;
+            this.btn_Distance.Text = "D";
+            this.btn_Distance.UseVisualStyleBackColor = true;
+            this.btn_Distance.Click += new System.EventHandler(this.btn_Distance_Click);
+            // 
+            // btn_Endpoint_1
+            // 
+            this.btn_Endpoint_1.Location = new System.Drawing.Point(93, 110);
+            this.btn_Endpoint_1.Name = "btn_Endpoint_1";
+            this.btn_Endpoint_1.Size = new System.Drawing.Size(40, 20);
+            this.btn_Endpoint_1.TabIndex = 11;
+            this.btn_Endpoint_1.Text = "E1";
+            this.btn_Endpoint_1.UseVisualStyleBackColor = true;
+            this.btn_Endpoint_1.Click += new System.EventHandler(this.btn_Endpoint_1_Click);
+            // 
+            // btn_Endpoint_2
+            // 
+            this.btn_Endpoint_2.Location = new System.Drawing.Point(139, 110);
+            this.btn_Endpoint_2.Name = "btn_Endpoint_2";
+            this.btn_Endpoint_2.Size = new System.Drawing.Size(40, 20);
+            this.btn_Endpoint_2.TabIndex = 12;
+            this.btn_Endpoint_2.Text = "E2";
+            this.btn_Endpoint_2.UseVisualStyleBackColor = true;
+            this.btn_Endpoint_2.Click += new System.EventHandler(this.btn_Endpoint_2_Click);
+            // 
+            // btn_About
+            // 
+            this.btn_About.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_About.Location = new System.Drawing.Point(7, 84);
+            this.btn_About.Name = "btn_About";
+            this.btn_About.Size = new System.Drawing.Size(80, 46);
+            this.btn_About.TabIndex = 13;
+            this.btn_About.Text = "About";
+            this.btn_About.UseVisualStyleBackColor = true;
+            this.btn_About.Click += new System.EventHandler(this.btn_About_Click);
+            // 
+            // lbl_Endpoint_1
+            // 
+            this.lbl_Endpoint_1.AutoSize = true;
+            this.lbl_Endpoint_1.Location = new System.Drawing.Point(185, 9);
+            this.lbl_Endpoint_1.Name = "lbl_Endpoint_1";
+            this.lbl_Endpoint_1.Size = new System.Drawing.Size(58, 13);
+            this.lbl_Endpoint_1.TabIndex = 14;
+            this.lbl_Endpoint_1.Text = "Endpoint 1";
+            // 
+            // lbl_Midpoint
+            // 
+            this.lbl_Midpoint.AutoSize = true;
+            this.lbl_Midpoint.Location = new System.Drawing.Point(185, 35);
+            this.lbl_Midpoint.Name = "lbl_Midpoint";
+            this.lbl_Midpoint.Size = new System.Drawing.Size(47, 13);
+            this.lbl_Midpoint.TabIndex = 15;
+            this.lbl_Midpoint.Text = "Midpoint";
+            // 
+            // lbl_Endpoint_2
+            // 
+            this.lbl_Endpoint_2.AutoSize = true;
+            this.lbl_Endpoint_2.Location = new System.Drawing.Point(185, 61);
+            this.lbl_Endpoint_2.Name = "lbl_Endpoint_2";
+            this.lbl_Endpoint_2.Size = new System.Drawing.Size(58, 13);
+            this.lbl_Endpoint_2.TabIndex = 16;
+            this.lbl_Endpoint_2.Text = "Endpoint 2";
+            // 
+            // lbl_Distance
+            // 
+            this.lbl_Distance.AutoSize = true;
+            this.lbl_Distance.Location = new System.Drawing.Point(185, 88);
+            this.lbl_Distance.Name = "lbl_Distance";
+            this.lbl_Distance.Size = new System.Drawing.Size(49, 13);
+            this.lbl_Distance.TabIndex = 17;
+            this.lbl_Distance.Text = "Distance";
+            // 
+            // Math_Assist
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 161);
+            this.Controls.Add(this.lbl_Distance);
+            this.Controls.Add(this.lbl_Endpoint_2);
+            this.Controls.Add(this.lbl_Midpoint);
+            this.Controls.Add(this.lbl_Endpoint_1);
+            this.Controls.Add(this.btn_About);
+            this.Controls.Add(this.btn_Endpoint_2);
+            this.Controls.Add(this.btn_Endpoint_1);
+            this.Controls.Add(this.btn_Distance);
+            this.Controls.Add(this.btn_Midpoint);
+            this.Controls.Add(this.txtbox_Y2);
+            this.Controls.Add(this.txtbox_X2);
+            this.Controls.Add(this.txtbox_YM);
+            this.Controls.Add(this.txtbox_XM);
+            this.Controls.Add(this.txtbox_Y1);
+            this.Controls.Add(this.txtbox_X1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Math_Assist";
+            this.Text = "Math Assist Version 0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbox_X1;
+        private System.Windows.Forms.TextBox txtbox_Y1;
+        private System.Windows.Forms.TextBox txtbox_XM;
+        private System.Windows.Forms.TextBox txtbox_YM;
+        private System.Windows.Forms.TextBox txtbox_X2;
+        private System.Windows.Forms.TextBox txtbox_Y2;
+        private System.Windows.Forms.Button btn_Midpoint;
+        private System.Windows.Forms.Button btn_Distance;
+        private System.Windows.Forms.Button btn_Endpoint_1;
+        private System.Windows.Forms.Button btn_Endpoint_2;
+        private System.Windows.Forms.Button btn_About;
+        private System.Windows.Forms.Label lbl_Endpoint_1;
+        private System.Windows.Forms.Label lbl_Midpoint;
+        private System.Windows.Forms.Label lbl_Endpoint_2;
+        private System.Windows.Forms.Label lbl_Distance;
+    }
+}
+
---
+++ Math_Assist/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Math_Assist
+{
+    public partial class Math_Assist : Form
+    {
+        public Math_Assist()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Distance_Click(object sender, EventArgs e)
+        {
+            double diff_X = Convert.ToDouble(txtbox_X2.Text) - Convert.ToDouble(txtbox_X1.Text);
+            double diff_Y = Convert.ToDouble(txtbox_Y2.Text) - Convert.ToDouble(txtbox_Y1.Text);
+            double distance = Math.Round(Math.Sqrt((diff_X * diff_X) + (diff_Y * diff_Y)), 3);
+            lbl_Distance.Text = "Distance: " + Convert.ToString(distance);
+        }
+
+        private void btn_Midpoint_Click(object sender, EventArgs e)
+        {
+            double X1 = Convert.ToDouble(txtbox_X1.Text);
+            double Y1 = Convert.ToDouble(txtbox_Y1.Text);
+            double X2 = Convert.ToDouble(txtbox_X2.Text);
+            double Y2 = Convert.ToDouble(txtbox_Y2.Text);
+            double XM = (X1 + X2) / 2;
+            double YM = (Y1 + Y2) / 2;
+            lbl_Midpoint.Text = Convert.ToString(Math.Round(XM, 2)) + ", " + Convert.ToString(Math.Round(YM, 2));
+        }
+
+        private void btn_Endpoint_1_Click(object sender, EventArgs e)
+        {
+            double X2 = Convert.ToDouble(txtbox_X2.Text);
+            double Y2 = Convert.ToDouble(txtbox_Y2.Text);
+            double XM = Convert.ToDouble(txtbox_XM.Text);
+            double YM = Convert.ToDouble(txtbox_YM.Text);
+            double X1 = (2 * XM) - X2;
+            double Y1 = (2 * YM) - Y2;
+            lbl_Endpoint_1.Text = X1 + ", " + Y1;
+        }
+
+        private void btn_Endpoint_2_Click(object sender, EventArgs e)
+        {
+            double X1 = Convert.ToDouble(txtbox_X1.Text);
+            double Y1 = Convert.ToDouble(txtbox_Y1.Text);
+            double XM = Convert.ToDouble(txtbox_XM.Text);
+            double YM = Convert.ToDouble(txtbox_YM.Text);
+            double X2 = (2 * XM) - X1;
+            double Y2 = (2 * YM) - Y1;
+            lbl_Endpoint_2.Text = X2 + ", " + Y2;
+        }
+
+        private void btn_About_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Math Assist V0.1, Created by Caleb E. Grebill - 2/10/2021");
+        }
+    }
+}
